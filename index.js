0025f965f8057862ff24d68848e53d8378ce2886
Michael Satran <michael.satran@outlook.com>
2018-10-27 23:35:00 -0700

    Stuff

---
+++ index.js
@@ -0,0 +1,22 @@
+//var http = require('http');
+
+// var server = http.createServer(function(request, response) {
+
+//     response.writeHead(200, {"Content-Type": "text/plain"});
+//     response.end("Hello World!");
+
+// });
+
+var express = require('express');
+var app = express();
+var path = require('path');
+
+app.get('/', function(req, res)
+{
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+var port = process.env.PORT || 1337;
+app.listen(port);
+
+console.log("Server running at http://localhost:%d", port);
