634fa6cc3dd4171e3703e14600416d89bc76df04
Michael Satran <michael.satran@outlook.com>
2018-10-28 04:45:55 -0700

    Finished implementation

--- index.js
+++ index.js
@@ -10,14 +10,22 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var fs = require('fs');
 
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res)
 {
+    var obj = JSON.parse(fs.readFileSync('devices.json', 'utf8'))
     //res.sendFile(path.join(__dirname + '/index.html'));
-    res.render('pages/index')
+    res.render('pages/index', {devices: obj})
+});
+
+app.get('/jobs', function(req, res)
+{
+    var obj = JSON.parse(fs.readFileSync('ratman.json', 'utf8'));
+    res.render('pages/jobs', {jobs: obj})
 });
 
 var port = process.env.PORT || 1337;
